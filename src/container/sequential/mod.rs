443408ccfe5f0838e005e90ed461f61feb7946e3
Nicolas Denoyelle <ndenoyelle@anl.gov>
2021-02-26 20:13:59 -0600

    initial commit

--- src/container/sequential/mod.rs
+++ src/container/sequential/mod.rs
@@ -8,4 +8,12 @@ mod stack;
 pub use crate::container::sequential::stack::Stack;
 mod top_k;
 pub use crate::container::sequential::top_k::TopK;
+/// Reading and writing objects.
+///
+/// ## Details
+///
+/// The IO module provides the abstractions for reading and writing objects
+/// in mediums other than main memory.
+/// ```
+mod io;
 mod tests;
