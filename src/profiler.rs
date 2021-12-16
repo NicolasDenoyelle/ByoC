a12d5ccedc700133f870e06dbb22d78715347422
ndenoyelle <ndenoyelle@anl.gov>
2021-12-15 20:59:56 -0600

    Fix doc

--- src/profiler.rs
+++ src/profiler.rs
@@ -116,7 +116,7 @@ impl Stats {
 }
 
 /// Time a function call and return `(time, output)`
-/// where `time` is elapsed time in milliseconds and
+/// where `time` is elapsed time in nanoseconds and
 /// `output` is the function call output.
 macro_rules! time_it {
     ($call:expr) => {{
@@ -202,56 +202,56 @@ impl<C> Profiler<C> {
 
     /// Get a summary of (0) the number of
     /// [`count()`](trait.BuildingBlock.html#tymethod.count) method call
-    /// and (1) the total time spent in milliseconds in these calls.
+    /// and (1) the total time spent in nanoseconds in these calls.
     pub fn count_stats(&self) -> (u64, u64) {
         self.stats.count.read()
     }
     /// Get a summary of (0) the number of
     /// [`contain()`](trait.BuildingBlock.html#tymethod.contain) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn contains_stats(&self) -> (u64, u64) {
         self.stats.contains.read()
     }
     /// Get a summary of (0) the number of
     /// [`take()`](trait.BuildingBlock.html#tymethod.take) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn take_stats(&self) -> (u64, u64) {
         self.stats.take.read()
     }
     /// Get a summary of (0) the number of
     /// [`pop()`](trait.BuildingBlock.html#tymethod.pop) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn pop_stats(&self) -> (u64, u64) {
         self.stats.pop.read()
     }
     /// Get a summary of (0) the number of
     /// [`push()`](trait.BuildingBlock.html#tymethod.push) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn push_stats(&self) -> (u64, u64) {
         self.stats.push.read()
     }
     /// Get a summary of (0) the number of
     /// [`flush()`](trait.BuildingBlock.html#tymethod.flush) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn flush_stats(&self) -> (u64, u64) {
         self.stats.flush.read()
     }
     /// Get a summary of (0) the number of iterations performed on an
     /// iterator obtained through
     /// [`flush()`](trait.BuildingBlock.html#tymethod.flush) method
-    /// and (1) the total time spent in milliseconds on iterations.
+    /// and (1) the total time spent in nanoseconds on iterations.
     pub fn flush_iter_stats(&self) -> (u64, u64) {
         self.stats.flush_iter.read()
     }
     /// Get a summary of (0) the number of
     /// [`get()`](trait.Get.html#tymethod.get) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn get_stats(&self) -> (u64, u64) {
         self.stats.get.read()
     }
     /// Get a summary of (0) the number of
     /// [`get_mut()`](trait.Get.html#tymethod.get_mut) method
-    /// call and (1) the total time spent in milliseconds in these calls.
+    /// call and (1) the total time spent in nanoseconds in these calls.
     pub fn get_mut_stats(&self) -> (u64, u64) {
         self.stats.get_mut.read()
     }
