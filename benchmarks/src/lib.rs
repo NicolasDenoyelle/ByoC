05c679330f8e3d8476b7144237829fc5b4d80336
Nicolas Denoyelle <denoyelle.nicolas@gmail.com>
2022-11-20 10:21:29 -0600

    remove wip benchmarks

--- benchmarks/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#[macro_use]
-extern crate clap;
-
-#[macro_use]
-mod microbenchmarks;
-pub use microbenchmarks::MicroBenchmark;
-
-mod microbenchmarks_args;
-pub use microbenchmarks_args::MicroBenchmarkArgs;
